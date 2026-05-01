1b262817345ebb483a9332d21bdb765dbc499396
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-05-01 21:19:55 +0500

    fix: qr code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://backend-1-6vfo.onrender.com/api/v1/',
+    // localhost-ni o'chirib, Render linkini qo'ying
+    baseURL: 'https://backend-1-6vfo.onrender.com/api/v1/menus', 
     headers: { 'Content-Type': 'application/json' }
 })
