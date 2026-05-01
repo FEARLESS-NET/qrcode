34f39fde850c47334a78fb31009a4ebbdbeb61d4
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-05-01 21:30:28 +0500

    fix menyus

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
     // localhost-ni o'chirib, Render linkini qo'ying
-    baseURL: 'https://backend-1-6vfo.onrender.com/api/v1/menus', 
+    baseURL: 'https://backend-1-6vfo.onrender.com/api/v1/', 
     headers: { 'Content-Type': 'application/json' }
 })
