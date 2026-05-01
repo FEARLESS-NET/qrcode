e36842542a35cb1daf2bbd8cbd91e3853b1d8ed5
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-05-01 21:05:39 +0500

    fix vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/',
+    baseURL: 'https://backend-1-6vfo.onrender.com/api/v1/',
     headers: { 'Content-Type': 'application/json' }
 })
