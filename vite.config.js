a7371c4fcc2f7ab2adf36c0f90ae5e8e0bbae4df
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-06-22 21:42:15 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
- 
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // React core (eng katta qism)
+            if (id.includes('/react/') || id.includes('/react-dom/')) {
+              return 'react-core';
+            }
+            // React Router
+            if (id.includes('react-router')) {
+              return 'react-router';
+            }
+            // UI kutubxona
+            if (id.includes('@material-tailwind')) {
+              return 'ui';
+            }
+            // Axios
+            if (id.includes('axios')) {
+              return 'axios';
+            }
+            // Qolgan barcha node_modules
+            return 'vendor';
+            
+          }
+        },
+      },
+    },
   },
 });
- 
