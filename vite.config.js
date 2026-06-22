2b0cff3e5dd3b1521253420a47a34155791375a9
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-06-22 21:47:29 +0500

    vite config1

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,14 +7,11 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
+      external: ['react', 'react-dom'],
       output: {
-        // ✅ MANUAL CHUNKS NI O'CHIRIB QO'YAMIZ (VAQTINCHA)
-        // manualChunks: {},
-        
-        // YOKI FAQAT KATTA PAKETLARNI AJRATAMIZ
-        manualChunks: {
-          // React ni alohida (lekin boshqa hech narsa)
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
